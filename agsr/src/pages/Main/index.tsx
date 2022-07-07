46a2e832e40f792cef659d1a3fbb6fd4781a4c3d
lost-fox <Oljaburak@mail.ru>
2022-07-07 23:06:19 +0300

    fix: branch info block

--- agsr/src/pages/Main/index.tsx
+++ agsr/src/pages/Main/index.tsx
@@ -1,5 +1,3 @@
-import mainImg from "../../assets/img/info-background.png";
-import { Search } from "../../components/Search";
 import { useAppDispatch } from "../../hooks/stateHooks";
 import { setRegistries } from "../../state/registriesSlice";
 import registries from "../../data/registries.json";
@@ -15,6 +13,7 @@ import { Registries } from "./Registries";
 import { News } from "./News";
 import { Docs } from "./Docs";
 import { Questions } from "./Question";
+import { Info } from "./Info";
 
 export const Main: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -28,35 +27,7 @@ export const Main: React.FC = () => {
 
   return (
     <div className="main-container">
-      <div className="main-info flex">
-        <div className="main-info-left">
-          <h1 className="main-info-left__title">
-            РЕЕСТР ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ
-          </h1>
-          <p className="main-info-left__text">
-            Единый реестр программ для электронных вычислительных машин и баз
-            данных
-          </p>
-          <p className="main-info-left__text">
-            Включено ПО в реестр:{" "}
-            <span className="main-info-left__text--blue">13 438</span>
-            <br />
-            Правообладателей:{" "}
-            <span className="main-info-left__text--blue">4 272</span>
-          </p>
-          <Search
-            divClassName="main-search"
-            imgClassName="main-search__icon"
-            inputClassName="main-search__input"
-            placeholder="Искать реестр..."
-          >
-            <button className="button main-search__button">Искать</button>
-          </Search>
-        </div>
-        <div className="main-info-right">
-          <img src={mainImg} alt="main background img" />
-        </div>
-      </div>
+      <Info />
       <Registries />
       <News />
       <Docs />
